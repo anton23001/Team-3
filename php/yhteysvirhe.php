bd72efe383dc1431193e3fc9e01dc56f27c4e4b1
anton23001 <142879285+anton23001@users.noreply.github.com>
2024-02-06 17:58:02 +0200

    Lisätty yhteysvirhe html+php

---
+++ php/yhteysvirhe.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "password";
+$database = "user_data";
+
+$yhteys = new mysqli($servername, $username, $password, $database);
+
+if ($yhteys->connect_error) {
+    include('../templates/yhteysvirhe.html');
+    die("Yhteysvirhe: " . $yhteys->connect_error);
+} else {
+    echo "Yhteys onnistui!";
+}
+
+$yhteys->close();
+?>
