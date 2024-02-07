80deca96684f7545a980b3c0e0ed8f3168ea8bb3
anton23001 <142879285+anton23001@users.noreply.github.com>
2024-02-07 09:33:33 +0200

    vaihdettu kansionimeä selkeämmäksi
    ml. tiedostot

--- php/yhteysvirhe.php
+++ php/yhteysvirhe.php
@@ -7,7 +7,7 @@ $database = "user_data";
 $yhteys = new mysqli($servername, $username, $password, $database);
 
 if ($yhteys->connect_error) {
-    include('../templates/yhteysvirhe.html');
+    include('../html/yhteysvirhe.html');
     die("Yhteysvirhe: " . $yhteys->connect_error);
 } else {
     echo "Yhteys onnistui!";
